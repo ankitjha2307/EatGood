94455fc3d260e001c26e77e14e88e8efe3a8f95d
ankit <ankitjha0723@gmail.com>
2025-07-11 21:25:02 +0530

    install react

---
+++ dist/EatGood - The Food Delivery App.6bd02f5a.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("h1", {
+    id: "title"
+}, "Ankit Jha");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=EatGood - The Food Delivery App.6bd02f5a.js.map
