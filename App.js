5238c8e35d1fa90b1b3378fe3db7d5dc5b8a7465
ankit <ankitjha0723@gmail.com>
2025-07-11 12:43:47 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+const heading = React.createElement(
+  "h1",
+  {
+    id: "title",
+  },
+  "Ankit Jha"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
