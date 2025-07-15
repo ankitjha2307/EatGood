149d994bd6b74555c3952abd58d1bd828f222a30
ankit <ankitjha0723@gmail.com>
2025-07-15 12:13:14 +0530

    start and build

--- App.js
+++ App.js
@@ -1,15 +1,2 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement(
-  "h1",
-  {
-    id: "title",
-  },
-  "Ankit Jha"
-);
-
-// kuch nhi kra aaj to
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(heading);
--- dist/EatGood - The Food Delivery App.cb65f72e.js
+++ dist/EatGood - The Food Delivery App.cb65f72e.js
@@ -679,11 +679,6 @@ var _react = require("react");
 var _reactDefault = parcelHelpers.interopDefault(_react);
 var _client = require("react-dom/client");
 var _clientDefault = parcelHelpers.interopDefault(_client);
-const heading = /*#__PURE__*/ (0, _reactDefault.default).createElement("h1", {
-    id: "title"
-}, "Ankit Jha");
-const root = (0, _clientDefault.default).createRoot(document.getElementById("root"));
-root.render(heading);
 
   $parcel$ReactRefreshHelpers$4931.postlude(module);
 } finally {
